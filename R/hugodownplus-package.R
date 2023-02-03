6a4863447329f4e9e8a51179fb05de94821cd61d
TimTeaFan <github@e-niversity.com>
2023-02-03 23:42:54 +0100

    fix: add package documentation and import params

---
+++ R/hugodownplus-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' @import rlang
+#' @import fs
+
+"_PACKAGE"
+
+hugodown <- new_environment()
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
