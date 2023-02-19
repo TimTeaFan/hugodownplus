99e9869ede89f28c123791593366d648dd60f9fb
TimTeaFan <github@e-niversity.com>
2023-02-19 14:06:20 +0100

    feat: add `pkgs` argument to `child_session_info()`

--- R/child_session_info.R
+++ R/child_session_info.R
@@ -3,6 +3,13 @@
 #' When called in an Rmd document this function generates child document containing an expandable
 #' session info box.
 #'
+#' @param pkgs Which packages to show. It may be:
+#'   * `NULL` or `"loaded"`: show all loaded packages,
+#'   * `"attached"`: show all attached packages,
+#'   * `"installed"`: show all installed packages,
+#'   * a character vector of package names. Their (hard) dependencies are
+#'     also shown by default, see the `dependencies` argument.
+#'
 #' @section Examples:
 #'
 #' Call this function on a separate line using inline code `{r, eval = FALSE, echo = TRUE}r child_session_info()`.
@@ -13,9 +20,9 @@
 #' Use `.session-details` to target the content of the box.
 #'
 #' @export
-child_session_info <- function() {
+child_session_info <- function(pkgs = c("loaded", "attached", "installed")[1]) {
   knitr::knit_child(fs::path_package("rmdtmp/session_info.Rmd",
                                      package = "hugodownplus"),
-                    envir = environment(),
+                    envir = environment(pkgs = pkgs),
                     quiet = TRUE)
 }
